19b76b752495d7002bb1745e7a9ca3c581c0a87c
Thang6797 <thangnguyenquoc6797@gmail.com>
2019-01-21 01:37:51 +0700

    Show all categories on admin page

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
   has_many :products
+
+  scope :newest_category, ->{order parent_id: :asc}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  root "static_pages#index"
+
   namespace :admin do
     root "static_pages#home"
     resources :users
+    resources :categories
   end
 end
